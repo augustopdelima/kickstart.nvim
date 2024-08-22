c427fc02d458c374520156335e83895ab69d3f80
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2024-08-22 20:06:30 -0300

    Change java_server lsp for jdtls

--- init.lua
+++ init.lua
@@ -578,7 +578,9 @@ require('lazy').setup({
         intelephense = {},
         html = {},
         cssls = {},
-        java_language_server = {},
+        jdtls = {
+          cmd = { 'jdtls' },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
